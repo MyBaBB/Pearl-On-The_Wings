dec1f3694c7eae0c862d77ef40f7cb0dc53b3818
Brett Baker <GitBaBB@MyBaBB.com>
2024-11-21 08:28:27 -0800

    added image

--- src/components/Pearls200.jsx
+++ src/components/Pearls200.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import Pearls200 from "../assets/Pearls200.jpg";
- 
+import "./Pearls200.css"
 
 function TrumpsArt() {
-   
-
-  return (
+     return (
     <div>
       {" "}
       <div className="Crystals200Wrapper   relative flex justify-center custom-landscape: mt-[12rem]
                       xxs:mt-[8rem] xs:mt-[5rem] sm:mt-0
                       scale-1 xxl:scale-150">
-        <div className="pearls200    ">
+          <div className="pearls200    ">
           <img
             src={Pearls200}
             alt="Image of Beloved Crystal"
             className="pearls200Img"
             width="400"
-            
-          />
+           />
         </div>
       </div>
     </div>
